43c836bc22366059d69d118fcaf884487109ddf3
jvis <ctmdev03@gmail.com>
2024-11-16 02:20:26 +0700

    test

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+// Hàm tính tổng
+function tinhTong($a, $b) {
+    return $a + $b;
+}
+
+// Sử dụng hàm
+$so1 = 5;
+$so2 = 10;
+
+$tong = tinhTong($so1, $so2);
+echo "Tổng của $so1 và $so2 là: $tong";
+?>
